a98b799a82a1ae7c2da17fb69b7962061ce9efab
DiegoContreras25 <133098011+DiegoContreras25@users.noreply.github.com>
2023-12-23 23:47:14 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -3,16 +3,15 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { Query } from "./resolvers/query.ts";
 import { Mutation } from "./resolvers/mutation.ts";
 import { typeDefs } from "./gql/schema.ts";
-import mongoose from "mongoose";
+import montoose from "mongoose";
 
-const MONGO_URL: string | undefined = Deno.env.get("MONGO_URL");
-
+const MONGO_URL = Deno.env.get("MONGO_URL");
 if (!MONGO_URL) {
-  console.log("Url not found in env");
-  Deno.exit(0);
+  throw new Error("Please provide a MongoDB connection string");
 }
 
-mongoose.connect(MONGO_URL);
+// Connect to MongoDB
+await montoose.connect(MONGO_URL);
 
 const server = new ApolloServer({
   typeDefs,
